66db8dee85f8d26ec6293d6c7eaba9d033f5f029
Matthew Roach <matthew@matthewroach.me>
2015-02-21 12:19:40 +0000

    Initial commit of theme
    This is the current theme used on my personal website.

---
+++ content-single.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * @package matthewroach
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<div class="home">
+		<a href="http://matthewroach.me/">Back to home</a>
+	</div>
+	<header class="entry-header">
+		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+	</header>
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'matthewroach' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div>
+
+	<footer class="entry-footer">
+		<?php
+			/* translators: used between list items, there is a space after the comma */
+			$category_list = get_the_category_list( __( ' ', 'matthewroach' ) );
+
+			/* translators: used between list items, there is a space after the comma */
+			$tag_list = get_the_tag_list( '', __( ', ', 'matthewroach' ) );
+
+			if ( ! matthewroach_categorized_blog() ) {
+				// This blog only has 1 category so we just need to worry about tags in the meta text
+				if ( '' != $tag_list ) {
+					$meta_text = __( 'This entry was tagged %2$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'matthewroach' );
+				} else {
+					$meta_text = __( 'Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'matthewroach' );
+				}
+			} else {
+				// But this blog has loads of categories so we should probably display them here
+				if ( '' != $tag_list ) {
+					$meta_text = __( 'This entry was posted in %1$s and tagged %2$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'matthewroach' );
+				} else {
+					$meta_text = __( 'This entry was posted in %1$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'matthewroach' );
+				}
+			} // end check for categories on this blog
+
+			// printf(
+			// 	$meta_text,
+			// 	$category_list,
+			// 	$tag_list,
+			// 	get_permalink()
+			// );
+		?>
+			<p class="cat-links">
+				<?php matthewroach_posted_on(); ?>
+				<?php printf( __( '%1$s', 'matthewroach' ), $category_list ); ?>
+			</p>
+
+		<?php edit_post_link( __( 'Edit', 'matthewroach' ), '<span class="edit-link">', '</span>' ); ?>
+	</footer>
+</article>
---
+++ footer.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package matthewroach
+ */
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+<!-- 			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'matthewroach' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'matthewroach' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( __( 'Theme: %1$s by %2$s.', 'matthewroach' ), 'matthewroach', '<a href="http://matthewroach.me" rel="designer">Matthew Roach</a>' ); ?> -->
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+<!--
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-163036-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+ -->
+</body>
+</html>
