8d48e7ced0c4b126b56bfbdf50ea2a80bee471bb
Matthew Roach <matthew@matthewroach.me>
2015-02-21 12:32:04 +0000

    Header and Menu
    Put header back in
    Remove menu button that’s on small screen
    Remove CSS for small screen menus, thats no longer needed

--- header.php
+++ header.php
@@ -26,19 +26,20 @@
 		<?php _e( 'Skip to content', 'matthewroach' ); ?>
 	</a>
 
-<!--
+
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
 			<h1 class="site-title">
-				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+					<?php bloginfo( 'name' ); ?>
+				</a>
 			</h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+			<!-- <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2> -->
 		</div>
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle"><?php _e( 'Primary Menu', 'matthewroach' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 		</nav>
-	</header> -->
+	</header>
 
 	<div id="content" class="site-content">
