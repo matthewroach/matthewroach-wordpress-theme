8090e970e81d6d09caad419d32ca8ebf37548d19
Matthew Roach <matthew@matthewroach.me>
2015-02-21 21:26:51 +0000

    Remove unused JavaScript

--- functions.php
+++ functions.php
@@ -94,10 +94,6 @@ add_action( 'widgets_init', 'matthewroach_widgets_init' );
 function matthewroach_scripts() {
 	wp_enqueue_style( 'matthewroach-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'matthewroach-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-
-	wp_enqueue_script( 'matthewroach-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
-
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
