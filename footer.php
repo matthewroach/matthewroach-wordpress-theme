fc27588ca952abd48350d0aa96a81b9266428e40
Matthew Roach <matthew@matthewroach.me>
2015-02-21 12:30:50 +0000

    Uncomment out Analytics code

--- footer.php
+++ footer.php
@@ -20,7 +20,7 @@
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
-<!--
+
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -31,6 +31,6 @@
   ga('send', 'pageview');
 
 </script>
- -->
+
 </body>
 </html>
