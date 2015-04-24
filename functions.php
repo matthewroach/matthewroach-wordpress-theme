f1d1636ab76173c65f040ccbeba4c71fa8f2dcf8
Matthew Roach <matthew@matthewroach.me>
2015-04-24 22:11:10 +0100

    Add Gallery post format

--- functions.php
+++ functions.php
@@ -58,7 +58,7 @@ function matthewroach_setup() {
 	 * See http://codex.wordpress.org/Post_Formats
 	 */
 	add_theme_support( 'post-formats', array(
-		'aside', 'image', 'video', 'quote', 'link',
+		'aside', 'image', 'video', 'quote', 'link', 'gallery'
 	) );
 
 	// Setup the WordPress core custom background feature.
