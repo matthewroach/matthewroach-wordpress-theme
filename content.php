c2b04a1d8248ca6367765ae2f75e462d3355705d
Matthew Roach <matthew@matthewroach.me>
2015-04-24 22:11:23 +0100

    code format updates

--- content.php
+++ content.php
@@ -20,26 +20,28 @@
 	</div>
 
 	<footer class="entry-footer">
-		<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
-			<?php
+		<?php
+			if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search
+
 				/* translators: used between list items, there is a space after the comma */
 				$categories_list = get_the_category_list( __( ' ', 'matthewroach' ) );
 				if ( $categories_list && matthewroach_categorized_blog() ) :
-			?>
-			<?php endif; // End if categories ?>
 
-			<?php
+				endif; // End if categories
+
 				/* translators: used between list items, there is a space after the comma */
 				$tags_list = get_the_tag_list( '', __( ', ', 'matthewroach' ) );
 				if ( $tags_list ) :
+
 			?>
 			<span class="tags-links">
 				<?php printf( __( 'Tagged %1$s', 'matthewroach' ), $tags_list ); ?>
 			</span>
-			<?php endif; // End if $tags_list ?>
-		<?php endif; // End if 'post' == get_post_type() ?>
+		<?php
+				endif; // End if $tags_list
+			endif; // End if 'post' == get_post_type()
 
-		<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
+		if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
 		<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'matthewroach' ), __( '1 Comment', 'matthewroach' ), __( '% Comments', 'matthewroach' ) ); ?></span>
 		<?php endif; ?>
 
