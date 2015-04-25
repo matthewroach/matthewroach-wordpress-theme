54db2473cea835575a887f5de08e20ee76d07b98
Matthew Roach <matthew@matthewroach.me>
2015-04-25 22:49:23 +0100

    Update Styles for galleries and JS
    Created a little JavaScript object to handle viewing the images in an
    overlay on the screen

--- functions.php
+++ functions.php
@@ -94,6 +94,8 @@ add_action( 'widgets_init', 'matthewroach_widgets_init' );
 function matthewroach_scripts() {
 	wp_enqueue_style( 'matthewroach-style', get_stylesheet_uri() );
 
+	wp_enqueue_script( 'theme-js', get_template_directory_uri() . '/js/gallery.js', array(), null, true );
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
