876f5d953ce80d1f3a345c4b2899663569d1968a
Matthew Roach <matthew@matthewroach.me>
2015-11-04 21:59:00 +0000

    Fix single article link
    Change the link for single article view to use the get link url
    function so the link post type url is the link of the title rather than
    the blog permalink

--- content-single.php
+++ content-single.php
@@ -7,7 +7,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 	<header class="entry-header">
-		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( matthewroach_get_link_url() ) ), '</a></h1>' ); ?>
 	</header>
 
 	<div class="entry-content">
