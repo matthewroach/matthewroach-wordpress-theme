1aca1742d2f41cb5db4064a21fecd34559d272d7
Matthew Roach <matthew@matthewroach.me>
2016-02-20 11:19:18 +0000

    Update home page
    Latest article at top, then status/link post, then instagram feed. Then
    more articles below that

--- home.php
+++ home.php
@@ -11,6 +11,41 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 		<!--
+			The Latest Article
+		-->
+		<?php $args = array(
+						'posts_per_page' => 1,
+						'post_type' => 'post',
+						'post_status' => 'publish',
+						'order' => 'DESC',
+						'tax_query' => array(
+							array(
+								'taxonomy' => 'post_format',
+								'field' => 'slug',
+								'terms' => array('post-format-quote','post-format-audio','post-format-gallery','post-format-image','post-format-link','post-format-status'),
+								'operator' => 'NOT IN'
+							),
+							array(
+							'taxonomy' => 'category',
+							'field' => 'slug',
+							'terms' => array( 'links', 'status', 'instagram' ),
+							'operator' => 'NOT IN'
+						)
+						)
+				  );
+
+			$query = new WP_Query( $args );
+
+			if ( $query->have_posts() ) {
+				while ( $query->have_posts() ) {
+					$query->the_post();
+					get_template_part( 'content', get_post_format() );
+				}
+			}
+
+		?>
+
+		<!--
 			Status
 		-->
 		<div class="status">
@@ -87,10 +122,11 @@ get_header(); ?>
 
 
 		<!--
-			Latest Article
+			Next 3 Articles
 		-->
 		<?php $args = array(
-						'posts_per_page' => 1,
+						'posts_per_page' => 3,
+						'offset' => 1,
 						'post_type' => 'post',
 						'post_status' => 'publish',
 						'order' => 'DESC',
@@ -129,7 +165,7 @@ get_header(); ?>
 
 		<?php $args = array(
 						'posts_per_page' => 10,
-						'offset' => 1,
+						'offset' => 4,
 						'post_type' => 'post',
 						'post_status' => 'publish',
 						'order' => 'DESC',
