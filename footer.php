6251f158f57fa8a141a79858413e38b437e3c211
Matthew Roach <matthew@matthewroach.me>
2016-02-20 10:57:11 +0000

    Wrap Analytics code in is_admin()

--- footer.php
+++ footer.php
@@ -4,6 +4,8 @@
  *
  * Contains the closing of the #content div and all content after
  *
+ * Wrapped Analytics code in is_admin() to only show if not logged in
+ *
  * @package matthewroach
  */
 ?>
@@ -14,16 +16,21 @@
 
 <?php wp_footer(); ?>
 
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-163036-1', 'auto');
-  ga('send', 'pageview');
+<?php if ( ! is_admin() ) { ?>
 
-</script>
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-163036-1', 'auto');
+		ga('send', 'pageview');
+
+	</script>
+
+<?php } ?>
 
 </body>
 </html>
