5fcb541bd0a3b42a6205dc1b9feb58b18a4eae92
Matthew Roach <matthew@matthewroach.me>
2017-04-23 23:30:07 +0100

    Change comments output to only show webmentions

--- comments.php
+++ comments.php
@@ -22,9 +22,10 @@ if ( post_password_required() ) {
 
 	<?php if ( have_comments() ) : ?>
 		<h2 class="comments-title">
+			Webmentions
 			<?php
-				printf( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'matthewroach' ),
-					number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
+				// printf( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'matthewroach' ),
+					// number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
 			?>
 		</h2>
 
@@ -39,7 +40,8 @@ if ( post_password_required() ) {
 		<ol class="comment-list">
 			<?php
 				wp_list_comments( array(
-					'style'      => 'ol',
+					'type'			 => 'webmention',
+					'style'      => 'ul',
 					'short_ping' => true,
 				) );
 			?>
@@ -55,14 +57,6 @@ if ( post_password_required() ) {
 
 	<?php endif; // have_comments() ?>
 
-	<?php
-		// If comments are closed and there are comments, let's leave a little note, shall we?
-		if ( ! comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
-	?>
-		<!-- <p class="no-comments"> -->
-			<?php // _e( 'Comments are closed.', 'matthewroach' ); ?>
-		<!-- </p> -->
-	<?php endif; ?>
 
 	<?php comment_form(); ?>
 
