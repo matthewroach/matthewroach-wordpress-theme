ab940a8f94729d1027506cb8957689049278eff6
Matthew Roach <matthew@matthewroach.me>
2017-04-26 22:54:59 +0100

    Update home page to only separate instagram posts from the main stream

--- home.php
+++ home.php
@@ -10,27 +10,18 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<!--
-			The Latest Article
-		-->
 		<?php $args = array(
-						'posts_per_page' => 1,
+						'posts_per_page' => 4,
 						'post_type' => 'post',
 						'post_status' => 'publish',
 						'order' => 'DESC',
 						'tax_query' => array(
 							array(
-								'taxonomy' => 'post_format',
-								'field' => 'slug',
-								'terms' => array('post-format-quote','post-format-audio','post-format-gallery','post-format-image','post-format-link','post-format-status'),
-								'operator' => 'NOT IN'
-							),
-							array(
 							'taxonomy' => 'category',
 							'field' => 'slug',
-							'terms' => array( 'links', 'status', 'instagram' ),
+							'terms' => array( 'instagram' ),
 							'operator' => 'NOT IN'
-						)
+						 )
 						)
 				  );
 
@@ -44,43 +35,6 @@ get_header(); ?>
 			}
 
 		?>
-
-		<!--
-			Status
-		-->
-		<div class="status">
-
-		<?php
-
-			$args = array(
-						'posts_per_page' => 1,
-						'post_type' => 'post',
-						'post_status' => 'publish',
-						'order' => 'DESC',
-						'tax_query' => array(
-							array(
-								'taxonomy' => 'post_format',
-								'field' => 'slug',
-								'terms' => array('post-format-status','post-format-link'),
-								'operator' => 'IN'
-							)
-						)
-		  );
-
-			$query = new WP_Query( $args );
-
-			// The Loop
-			if ( $query->have_posts() ) {
-				while ( $query->have_posts() ) {
-					$query->the_post();
-					get_template_part( 'content', get_post_format() );
-				}
-			}
-
-			wp_reset_postdata();
-		?>
-
-		</div>
 
 
 		<!--
@@ -122,27 +76,21 @@ get_header(); ?>
 
 
 		<!--
-			Next 3 Articles
+			Next 5 Articles
 		-->
 		<?php $args = array(
-						'posts_per_page' => 3,
-						'offset' => 1,
+						'posts_per_page' => 5,
+						'offset' => 4,
 						'post_type' => 'post',
 						'post_status' => 'publish',
 						'order' => 'DESC',
 						'tax_query' => array(
 							array(
-								'taxonomy' => 'post_format',
-								'field' => 'slug',
-								'terms' => array('post-format-quote','post-format-audio','post-format-gallery','post-format-image','post-format-link','post-format-status'),
-								'operator' => 'NOT IN'
-							),
-							array(
 							'taxonomy' => 'category',
 							'field' => 'slug',
-							'terms' => array( 'links', 'status', 'instagram' ),
+							'terms' => array( 'instagram' ),
 							'operator' => 'NOT IN'
-						)
+						 )
 						)
 				  );
 
@@ -159,29 +107,23 @@ get_header(); ?>
 
 
 		<!--
-			Next 10 Recent Article after the one above
+			Next 10 Recent Articles in sequence after the ones above
 		-->
 		<div class="post-listing">
 
 		<?php $args = array(
 						'posts_per_page' => 10,
-						'offset' => 4,
+						'offset' => 9,
 						'post_type' => 'post',
 						'post_status' => 'publish',
 						'order' => 'DESC',
 						'tax_query' => array(
 							array(
-								'taxonomy' => 'post_format',
-								'field' => 'slug',
-								'terms' => array('post-format-quote','post-format-audio','post-format-gallery','post-format-image','post-format-link','post-format-status'),
-								'operator' => 'NOT IN'
-							),
-							array(
 							'taxonomy' => 'category',
 							'field' => 'slug',
-							'terms' => array( 'links', 'status', 'instagram' ),
+							'terms' => array( 'instagram' ),
 							'operator' => 'NOT IN'
-						)
+						 )
 						)
 				  );
 
