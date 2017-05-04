feb6c9dcb00053006260081d0a5654f8ba15befe
Matthew Roach <matthew@matthewroach.me>
2017-05-04 11:10:03 +0100

    Add function to set title to date if blank - mainly for micro.blog posts

--- functions.php
+++ functions.php
@@ -126,3 +126,21 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+
+/*
+ * Simple function to replace the post title if blank - just add it to functions.php
+ *
+ * 'title_save_pre' runs against the post title before the post is saved
+ */
+
+
+function filter_title_save_pre( $title ) {
+    if ( $title == "" ) {
+      return date( 'd/m/Y, H:i' );
+    } else {
+      return $title;
+    }
+}
+
+add_filter( 'title_save_pre', 'filter_title_save_pre', 10, 1 );
